a076378cc212bf110b14333fad56cbffa882993f
sungchan98 <jvnhkdm0807@gmail.com>
2024-04-08 13:07:52 +0900

    0408

--- app/src/main/java/bitcamp/myapp/App.java
+++ app/src/main/java/bitcamp/myapp/App.java
@@ -1,5 +1,7 @@
 package bitcamp.myapp;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -10,15 +12,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 @EnableTransactionManagement
 @PropertySource({
-    "file:${user.home}/config/jdbc.properties",
-    "file:${user.home}/config/ncp.properties",
-    "file:${user.home}/config/ncp-secret.properties",
+    "file:${HOME}/config/jdbc.properties",
+    "file:${HOME}/config/ncp.properties",
+    "file:${HOME}/config/ncp-secret.properties"
 })
 @Controller
 public class App {
 
+  private static Log log = LogFactory.getLog(App.class);
+
   public static void main(String[] args) throws Exception {
-    System.out.println("과제관리 시스템 서버 실행!");
+    log.info("과제관리 시스템 서버 실행!");
     SpringApplication.run(App.class, args);
   }
 
@@ -29,4 +33,5 @@ public class App {
   @GetMapping("/about")
   public void about() {
   }
+
 }
